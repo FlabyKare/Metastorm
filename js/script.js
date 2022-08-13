47d0ee3a33a16292e37862a8baeea2b44043542b
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-08-13 11:34:44 +0300

    first_slider_finished

--- js/script.js
+++ js/script.js
@@ -3,6 +3,27 @@ $('.slider_box_wrapper').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
-    dots: true
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false
+            }
+          },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
   });
       
