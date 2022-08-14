2b88f5e9f4f3b1a4107fa363a933037284d40a26
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-08-14 20:32:11 +0300

    sliders_finished

--- js/script.js
+++ js/script.js
@@ -1,29 +1,63 @@
-$('.slider_box_wrapper').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              arrows: false
-            }
-          },
-        {
-          breakpoint: 576,
-          settings: {
+$(".slider_box_wrapper").slick({
+   infinite: true,
+   slidesToShow: 3,
+   slidesToScroll: 1,
+   arrows: true,
+   dots: true,
+   responsive: [
+      {
+         breakpoint: 768,
+         settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
-            arrows: false
-          }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-  });
-      
+            arrows: false,
+         },
+      },
+      {
+         breakpoint: 576,
+         settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+         },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+   ],
+});
+
+$(".slider_gun_wrapper").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+ });
+ $(".mobile_slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+ });
+
+var swiper = new Swiper(".mySwiper", {
+   loop: false,
+   spaceBetween: 10,
+   slidesPerView: 9,
+   freeMode: true,
+   watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+   loop: true,
+   spaceBetween: 10,
+   slidesPerView: 1,
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   thumbs: {
+      swiper: swiper,
+   },
+});
